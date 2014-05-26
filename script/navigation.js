df6d514cf36f2a09042b7c1afd6ba412c2088e8c
Benedikt Schmidt <benediktibk@aon.at>
2014-05-26 16:32:12 +0200

    improved automatic scrolling

--- script/navigation.js
+++ script/navigation.js
@@ -6,26 +6,12 @@ $(function()
 	var $window = $(window);
 	$window.scroll(function() 
 		{
-			var scrollTop = $window.scrollTop();
-			var shouldBeFixed = scrollTop > navigationY;
-			if (shouldBeFixed && !isFixed) 
+			navigation.css(
 			{
-				navigation.css(
-				{
-					position: 'fixed',
-					top: 8,
-					left: navigation.offset().left,
-					width: navigation.width()
-				});
-				isFixed = true;
-			}
-			else if (!shouldBeFixed && isFixed)
-			{
-				navigation.css(
-				{
-					position: 'static'
-				});
-				isFixed = false;
-			}
+				position: 'fixed',
+				top: 0,
+				left: navigation.offset().left,
+				width: navigation.width()
+			});
 		});
 }); 

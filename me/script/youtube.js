2ea100b8b68123773f83918997c96931a2ee0bac
Benedikt Schmidt <benediktibk@outlook.com>
2020-06-25 07:39:55 +0200

    Improve Youtube-Preview

--- me/script/youtube.js
+++ me/script/youtube.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",
+/*document.addEventListener("DOMContentLoaded",
         function() {
             var div, n,
                 v = document.getElementsByClassName("youtube-player");
@@ -24,4 +24,4 @@ document.addEventListener("DOMContentLoaded",
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allowfullscreen", "1");
         this.parentNode.replaceChild(iframe, this);
-    }
+    });*/
